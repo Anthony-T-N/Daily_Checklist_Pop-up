8ae30effaceed9c7e64f543935f3de705a377763
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-01-20 14:43:21 +1030

    Update Form1.cs

--- Daily_Checklist_Pop-up/Form1.cs
+++ Daily_Checklist_Pop-up/Form1.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace Daily_Checklist_Pop_up
 {
     public partial class Form1 : Form
     {
-        private int _ticks = 0;
+        private int _ticks = 1000;
 
         public Form1()
         {
@@ -20,17 +21,27 @@ namespace Daily_Checklist_Pop_up
             day_countdown_timer.Tick += day_countdown_timer_Tick;
             day_countdown_timer.Interval = 1000;
             day_countdown_timer.Start();
+            countdown_calculations();
         }
 
         private void day_countdown_timer_Tick(object sender, EventArgs e)
         {
-            _ticks++;
+            _ticks--;
             this.day_countdown_timer_label.Text = _ticks.ToString();
             if (_ticks == 5)
             {
                 this.day_countdown_timer_label.Text = "DONE";
                 day_countdown_timer.Stop();
             }
+        }
+        private void countdown_calculations()
+        {
+            // Determine time left in current local day and apply countdown on window countdown label.
+            DateTime start_of_day = new DateTime();
+            var remaining = TimeSpan.FromHours(24) - start_of_day.TimeOfDay;
+            Debug.WriteLine(remaining);
+            DateTime startDate = DateTime.ParseExact(DateTime.Date, "yyyyMMdd");
+            Debug.WriteLine(startDate);
         }
     }
 }
