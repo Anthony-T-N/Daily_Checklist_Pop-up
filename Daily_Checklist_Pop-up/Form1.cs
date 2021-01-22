bfbfb3ff2a4c4d8a377ac8119eb6caecf643a279
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2021-01-22 15:47:45 +1030

    Update Form1.cs

--- Daily_Checklist_Pop-up/Form1.cs
+++ Daily_Checklist_Pop-up/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,7 @@ namespace Daily_Checklist_Pop_up
 {
     public partial class Form1 : Form
     {
-        private double _ticks;
+        private TimeSpan _ticks;
 
         public Form1()
         {
@@ -22,20 +22,34 @@ namespace Daily_Checklist_Pop_up
             day_countdown_timer.Tick += day_countdown_timer_Tick;
             day_countdown_timer.Interval = 1000;
             day_countdown_timer.Start();
-            countdown_calculations();
         }
 
         private void day_countdown_timer_Tick(object sender, EventArgs e)
         {
-            _ticks--;
-            this.day_countdown_timer_label.Text = _ticks.ToString();
-            if (_ticks <= 5)
+            _ticks.Subtract(TimeSpan.FromSeconds(1));
+            Debug.WriteLine(_ticks.ToString());
+            // this.day_countdown_timer_label.Text = _ticks.ToString();
+            this.day_countdown_timer_label.Text = string.Format("{0:HH:mm:ss}", _ticks.ToString());
+            if (_ticks.Seconds <= 0)
             {
                 this.day_countdown_timer_label.Text = "DONE";
                 day_countdown_timer.Stop();
+                List<CheckBox> checkBoxes = new List<CheckBox>()
+                {
+                    checkBox1,
+                    checkBox2,
+                    checkBox3,
+                    checkBox4,
+                    checkBox5,
+                    checkBox6
+                };
+                for (int i = 0; i <= checkBoxes.Count - 1; i++)
+                {
+                    checkBoxes[i].Checked = false;
+                }
             }
         }
-        private double countdown_calculations()
+        private TimeSpan countdown_calculations()
         {
             // Determine time left in current local day and apply countdown on window countdown label.
 
@@ -47,9 +61,11 @@ namespace Daily_Checklist_Pop_up
             */
             Debug.WriteLine(end_of_day.TimeOfDay.TotalHours);
             Debug.WriteLine(DateTime.Now.TimeOfDay.TotalHours);
-            var remaining_time = end_of_day.TimeOfDay.TotalHours - DateTime.Now.TimeOfDay.TotalHours;
-            Debug.WriteLine(remaining_time.ToString("HH:mm:ss tt"));
-            Debug.WriteLine(string.Format("{0:HH:mm:ss tt}", remaining_time));
+            TimeSpan remaining_time = end_of_day.TimeOfDay - DateTime.Now.TimeOfDay;
+            // Debug.WriteLine(remaining_time.ToString("HH:mm:ss"));
+            Debug.WriteLine(string.Format("{0:HH:mm:ss tt}", end_of_day));
+            Debug.WriteLine(string.Format("{0:hh\\:mm\\:ss}", remaining_time));
+           // remaining_time = string.Format("{0:HH:mm:ss tt}", remaining_time);
             return remaining_time;
         }
     }
